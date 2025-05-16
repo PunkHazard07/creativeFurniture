e393766b4c8b06ad3fa1e17e64251f6d9fd89b0b
Bello Ayoola Raji <belloayoola20@gmail.com>
2025-05-16 15:16:07 +0100

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,37 +1,7 @@
-// import React from 'react'
-// import { Router, Routes, Route } from 'react-router-dom'
-// import Login from './components/Login'
-// import Register from './components/Register'
-// import Dashboard from './pages/Dashboard'
-// import Add from './pages/Add'
-// import List from './pages/List'
-// import Orders from './pages/Orders'
 
-// const App = () => {
-//   return (
-//     <div>
-//       <Router>
-//         <Routes>
-//           {/* Auth Routes */}
-//           <Route path="/login" element={<Login />} />
-//           <Route path="/register" element={<Register />} />
-
-//           {/* Admin Panel Routes */}
-//           <Route path='/dashboard' element={<Dashboard />} />
-//           <Route path='/add' element={<Add />} />
-//           <Route path='/list' element={<List />} />
-//           <Route path='/orders' element={<Orders />} />
-//         </Routes>
-//       </Router>
-
-//     </div>
-//   )
-// }
-
-// export default App
 
 import React from 'react'
-import VerifyToken from './components/verifyToken'
+import VerifyToken from './components/VerifyToken'
 
 const App = () => {
   return (
